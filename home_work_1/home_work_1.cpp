d81d988539c459a60763ceace52b2d3ed316b8fe
TRIOLI LOW <trioli.low@gmail.com>
2022-12-20 02:45:35 +0300

    init

---
+++ home_work_1/home_work_1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <Windows.h>
+#define MODE 1 
+#if defined (MODE) 
+
+int add(int a,int  b) {
+	return a + b;
+}
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int a(0); int b(0);
+
+#if MODE == 0 
+	std::cout << "Работаю в режиме тренировки";
+#elif MODE == 1
+	std::cout << "Работаю в боевом режиме: " << std::endl;
+	std::cout << " Введите число 1: ";
+	std::cin >> a;
+	std::cout << " Введите число 2: ";
+	std::cin >> b;
+	std::cout << "Результат сложения: " << add(a, b);
+#else 
+	std::cout << "Неизвестный режим. Завершение работы!";
+
+
+#endif
+#endif
+#if !defined (MODE)
+	std::cout << "Error!";
+#endif
+}
